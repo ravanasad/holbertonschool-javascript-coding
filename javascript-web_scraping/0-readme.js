42fe8e2189125d2d877c9f8b093c1cb2ef0ec540
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-05-19 09:15:07 +0400

    Style

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+const FileName = process.argv[2];
+fs.readFile(FileName, 'utf8', function (err, line) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(line);
   }
 });
